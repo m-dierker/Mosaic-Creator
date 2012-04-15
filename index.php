41d29fa4458d46f512e90fed9efa274e3d25b5fb
Matthew Dierker <mvd7793@gmail.com>
2012-04-14 21:29:47 -0700

    Working on the upload plugin

--- index.php
+++ index.php
@@ -5,111 +5,233 @@
 <?php
 
 // Require functions.php, which includes things you need for facebook, and includes JQuery
-require_once('functions.php');
+require('functions.php');
+require('fileUploads.php');
 
 
 ?>
 
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>MP6 Mosaic Creator - Designed by Matthew Dierker</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Create beautiful mosaic images using your own pictures, free!">
-		<meta name="author" content="Matthew Dierker">
+<head>
+	<meta charset="utf-8">
+	<title>MP6 Mosaic Creator - Designed by Matthew Dierker</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Create beautiful mosaic images using your own pictures, free!">
+	<meta name="author" content="Matthew Dierker">
 
-		<!-- Le styles -->
-		<link href="css/bootstrap.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
-		<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<!-- Le styles -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
 
-		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+			<![endif]-->
 
-	</head>
+		</head>
 
-	<body>
+		<body>
 
-	<div class="navbar navbar-fixed-top">
-	 	<div class="navbar-inner">
-			<div class="container">
-				<!-- This is for mobile... I think -->
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-			  	<a class="brand" href="#">MP6.3 Mosaic Creator</a>
-			  	<div class="nav-collapse">
-					<ul class="nav">
-					 	<li class="active"><a href="#">Home</a></li>
-					 	<li class="dropdown">
-					 		<a class ="dropdown-toggle" data-toggle="dropdown" href="https://wiki.engr.illinois.edu/display/cs225/MP+6">Original MP Spec <b class="caret"></b></a>
-					 		<ul class="dropdown-menu">
-					 			<li><a href="https://wiki.engr.illinois.edu/display/cs225/MP+6.1">MP 6.1 (Part 1)</a></li>
-					 			<li><a href="https://wiki.engr.illinois.edu/display/cs225/MP+6.2">MP 6.2 (Part 2)</a></li>
-					 		</ul>
-					 	</li>
+			<div class="navbar navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container">
+						<!-- This is for mobile... I think -->
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+						<a class="brand" href="#">MP6.3 Mosaic Creator</a>
+						<div class="nav-collapse">
+							<ul class="nav">
+								<li class="active"><a href="#">Home</a></li>
+								<li class="dropdown">
+									<a class ="dropdown-toggle" data-toggle="dropdown" href="https://wiki.engr.illinois.edu/display/cs225/MP+6">Original MP Spec <b class="caret"></b></a>
+									<ul class="dropdown-menu">
+										<li><a href="https://wiki.engr.illinois.edu/display/cs225/MP+6.1">MP 6.1 (Part 1)</a></li>
+										<li><a href="https://wiki.engr.illinois.edu/display/cs225/MP+6.2">MP 6.2 (Part 2)</a></li>
+									</ul>
+								</li>
 
-					 	<!-- <li><a href="#contact">Contact</a></li> -->
-					</ul>
-			  	</div><!--/.nav-collapse -->
-			</div>
-	  	</div>
-	</div>
-
-	<div class="container-fluid">
-		<div class="row-fluid">
-			<div class="span8">
-				<div class="hero-unit">
-					<h1>Create your own Mosaic!</h1>
-					<p>Use your own images to make a mosaic, like these! <b>To get started, just login on the left.</b></p> 
-
-					<div class="onLoginHide onLogoutSlideIn hide">
-						<img src="img/mosaic-sample.png">&nbsp;&nbsp;&nbsp;<img src="img/mosaic-sample.png"><br><br>
-						<a class="btn btn-primary btn-large" href="img/mosaic.png">See them bigger!</a>
-					</div>
-					<div class="onLogoutHide onLoginSlideIn hide">
-						<a class="btn btn-primary btn-large" href="img/mosaic.png">See an example!</a>
-					</div>
-
-				</div>
-
-				<h2>Upload your Files</h2>
-
-
-				<form id="fileupload" action ="uploadPlugin/server/php" method="POST" enctype="multipart/form-data">
-				</form>
-
-
-
-			</div>
-			<div class="span4">
-				<div class="loginContainer">
-					<div class="onLoginHide onLogoutHide">
-						Loading... <img src="img/pacman-loader.gif">
-					</div>
-					<div class="loginBox onLoginHide onLogoutFadeIn hide" id="loginBox">
-						<h3>Login</h3>
-						<p>We'll need you to login to facebook to start! If you don't have a facebook account, you're out of luck for now. Sorry! (but feel free to <a href="http://facebook.com">register</a>!)</p>
-
-						<div class="fb-login-button">Login with Facebook</div>
-
-					</div>
-					<div class="logoutBox onLogoutHide onLoginFadeIn hide" id="logoutBox">
-						<h3>
-							Welcome <a class="fb-profile-link"><span class="fb-first-name"></span></a>!
-						</h3>
-						<p>
-							<a class="btn btn-mini" href="javascript:logout();">Logout</a>
-						</p>
+								<!-- <li><a href="#contact">Contact</a></li> -->
+							</ul>
+						</div><!--/.nav-collapse -->
 					</div>
 				</div>
 			</div>
-		</div>
-	</div> <!-- /container -->
+
+			<div class="container-fluid">
+				<div class="row-fluid">
+					<div class="span8">
+						<div class="hero-unit">
+							<h1>Create your own Mosaic!</h1>
+							<p>Use your own images to make a mosaic, like these! <b>To get started, just login on the left.</b></p> 
+
+							<div class="onLoginHide onLogoutSlideIn hide">
+								<img src="img/mosaic-sample.png">&nbsp;&nbsp;&nbsp;<img src="img/mosaic-sample.png"><br><br>
+								<a class="btn btn-primary btn-large" href="img/mosaic.png">See them bigger!</a>
+							</div>
+							<div class="onLogoutHide onLoginSlideIn hide">
+								<a class="btn btn-primary btn-large" href="img/mosaic.png">See an example!</a>
+							</div>
+
+						</div>
+
+						<div id="fileUpload" class="onLoginSlide hide">
+							<h2>Upload your Files</h2>
+
+
+							<form id="fileupload" action="upload-plugin/server/php/" method="POST" enctype="multipart/form-data">
+								<!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
+								<div class="row fileupload-buttonbar">
+									<div class="span7">
+										<!-- The fileinput-button span is used to style the file input field as button -->
+										<span class="btn btn-success fileinput-button">
+											<i class="icon-plus icon-white"></i>
+											<span>Add files...</span>
+											<input type="file" name="files[]" multiple>
+										</span>
+										<button type="submit" class="btn btn-primary start">
+											<i class="icon-upload icon-white"></i>
+											<span>Start upload</span>
+										</button>
+										<button type="reset" class="btn btn-warning cancel">
+											<i class="icon-ban-circle icon-white"></i>
+											<span>Cancel upload</span>
+										</button>
+										<button type="button" class="btn btn-danger delete">
+											<i class="icon-trash icon-white"></i>
+											<span>Delete</span>
+										</button>
+										<input type="checkbox" class="toggle">
+									</div>
+									<div class="span5">
+										<!-- The global progress bar -->
+										<div class="progress progress-success progress-striped active fade">
+											<div class="bar" style="width:0%;"></div>
+										</div>
+									</div>
+								</div>
+								<!-- The loading indicator is shown during image processing -->
+								<div class="fileupload-loading"></div>
+								<br>
+								<!-- The table listing the files available for upload/download -->
+								<table class="table table-striped"><tbody class="files" data-toggle="modal-gallery" data-target="#modal-gallery"></tbody></table>
+							</form>
+						</div>
+
+						<div id="modal-gallery" class="modal modal-gallery hide fade">
+						    <div class="modal-header">
+						        <a class="close" data-dismiss="modal">&times;</a>
+						        <h3 class="modal-title"></h3>
+						    </div>
+						    <div class="modal-body"><div class="modal-image"></div></div>
+						    <div class="modal-footer">
+						        <a class="btn modal-download" target="_blank">
+						            <i class="icon-download"></i>
+						            <span>Download</span>
+						        </a>
+						        <a class="btn btn-success modal-play modal-slideshow" data-slideshow="5000">
+						            <i class="icon-play icon-white"></i>
+						            <span>Slideshow</span>
+						        </a>
+						        <a class="btn btn-info modal-prev">
+						            <i class="icon-arrow-left icon-white"></i>
+						            <span>Previous</span>
+						        </a>
+						        <a class="btn btn-primary modal-next">
+						            <span>Next</span>
+						            <i class="icon-arrow-right icon-white"></i>
+						        </a>
+						    </div>
+						</div>
+						<!-- The template to display files available for upload -->
+						<script id="template-upload" type="text/x-tmpl">
+						{% for (var i=0, file; file=o.files[i]; i++) { %}
+						    <tr class="template-upload fade">
+						        <td class="preview"><span class="fade"></span></td>
+						        <td class="name"><span>{%=file.name%}</span></td>
+						        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+						        {% if (file.error) { %}
+						            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
+						        {% } else if (o.files.valid && !i) { %}
+						            <td>
+						                <div class="progress progress-success progress-striped active"><div class="bar" style="width:0%;"></div></div>
+						            </td>
+						            <td class="start">{% if (!o.options.autoUpload) { %}
+						                <button class="btn btn-primary">
+						                    <i class="icon-upload icon-white"></i>
+						                    <span>{%=locale.fileupload.start%}</span>
+						                </button>
+						            {% } %}</td>
+						        {% } else { %}
+						            <td colspan="2"></td>
+						        {% } %}
+						        <td class="cancel">{% if (!i) { %}
+						            <button class="btn btn-warning">
+						                <i class="icon-ban-circle icon-white"></i>
+						                <span>{%=locale.fileupload.cancel%}</span>
+						            </button>
+						        {% } %}</td>
+						    </tr>
+						{% } %}
+						</script>
+						<!-- The template to display files available for download -->
+						<script id="template-download" type="text/x-tmpl">
+						{% for (var i=0, file; file=o.files[i]; i++) { %}
+						    <tr class="template-download fade">
+						        {% if (file.error) { %}
+						            <td></td>
+						            <td class="name"><span>{%=file.name%}</span></td>
+						            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+						            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
+						        {% } else { %}
+						            <td class="preview">{% if (file.thumbnail_url) { %}
+						                <a href="{%=file.url%}" title="{%=file.name%}" rel="gallery" download="{%=file.name%}"><img src="{%=file.thumbnail_url%}"></a>
+						            {% } %}</td>
+						            <td class="name">
+						                <a href="{%=file.url%}" title="{%=file.name%}" rel="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
+						            </td>
+						            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+						            <td colspan="2"></td>
+						        {% } %}
+						        <td class="delete">
+						            <button class="btn btn-danger" data-type="{%=file.delete_type%}" data-url="{%=file.delete_url%}">
+						                <i class="icon-trash icon-white"></i>
+						                <span>{%=locale.fileupload.destroy%}</span>
+						            </button>
+						            <input type="checkbox" name="delete" value="1">
+						        </td>
+						    </tr>
+						{% } %}
+						</script>
+
+					</div>
+					<div class="span4">
+						<div class="loginContainer">
+							<div class="onLoginHide onLogoutHide">
+								Loading... <img src="img/pacman-loader.gif">
+							</div>
+							<div class="loginBox onLoginHide onLogoutFadeIn hide" id="loginBox">
+								<h3>Login</h3>
+								<p>We'll need you to login to facebook to start! If you don't have a facebook account, you're out of luck for now. Sorry! (but feel free to <a href="http://facebook.com">register</a>!)</p>
+
+								<div class="fb-login-button">Login with Facebook</div>
+
+							</div>
+							<div class="logoutBox onLogoutHide onLoginFadeIn hide" id="logoutBox">
+								<h3>
+									Welcome <a class="fb-profile-link"><span class="fb-first-name"></span></a>!
+								</h3>
+								<p>
+									<a class="btn btn-mini" href="javascript:logout();">Logout</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> <!-- /container -->
 
 	<!-- Le javascript
 	================================================== -->
@@ -118,30 +240,38 @@ require_once('functions.php');
 	<script src="js/bootstrap.min.js"></script>
 
 	<script type="text/javascript">
-    	var facebookName, facebookFirstName, facebookLastName, facebookID;
-    </script>
+	var facebookName, facebookFirstName, facebookLastName, facebookID;
+	</script>
 
 	<script type="text/javascript" src="js/home.js"></script>
 
 	<div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            appId      : <?php echo FACEBOOK_APP_ID; ?>,
-            status     : true, 
-            cookie     : true,
-            xfbml      : true,
-            oauth      : true,
-          });
+	<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : <?php echo FACEBOOK_APP_ID; ?>,
+			status     : true, 
+			cookie     : true,
+			xfbml      : true,
+			oauth      : true,
+		});
 
-          facebookInitComplete();
-        };
-        (function(d){
-           var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-           js = d.createElement('script'); js.id = id; js.async = true;
-           js.src = "//connect.facebook.net/en_US/all.js";
-           d.getElementsByTagName('head')[0].appendChild(js);
-         }(document));
-      </script>
-  </body>
+		facebookInitComplete();
+	};
+	(function(d){
+		var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+		js = d.createElement('script'); js.id = id; js.async = true;
+		js.src = "//connect.facebook.net/en_US/all.js";
+		d.getElementsByTagName('head')[0].appendChild(js);
+	}(document));
+	</script>
+
+
+
+	<!-- Start File Upload Scripts -->
+
+	<?php includeFileUploadScripts(); ?>
+
+	<!-- End File Upload Scripts -->
+</body>
 </html>
